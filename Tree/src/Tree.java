bcb14f5d06bbb25040977f431d8ee03456b76dd9
tanw33t3ck <145963565+tanw33t3ck@users.noreply.github.com>
2024-04-17 11:02:14 +0800

    Print a Tree Practice

---
+++ Tree/src/Tree.java
@@ -0,0 +1,14 @@
+
+public class Tree {
+    public static void main(String[] args) {
+        //this method will print a tree on the screen
+        System.out.println("Hi, I am WT and I am learning to code Java.");
+        System.out.println("Ready to get my hands dirty!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println("*****");
+        System.out.println("  *  ");
+        System.out.println("... And I am done!");
+
+    }
+}
